e651ab433f404dc49f5838f525e6136ad6950ab3
Aler Denisov <aler.zampillo@gmail.com>
2018-11-05 16:15:44 +0300

    simple padding container component

---
+++ components/templates/Container.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  section(:class='b()' :style='style')
+    slot
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "nuxt-property-decorator";
+
+@Component({
+  name: "t-container"
+})
+export default class extends Vue {
+  @Prop({ default: 10 })
+  padding: number;
+
+  get style() {
+    return {
+      padding: `${this.padding}px`
+    };
+  }
+}
+</script>
+
+
+<style lang="scss">
+.t-container {
+  padding: 10px;
+}
+</style>
