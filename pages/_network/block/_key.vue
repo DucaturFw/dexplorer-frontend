986eb664674fb893f72f8279a053c5bce09a2b5f
Aler Denisov <aler.zampillo@gmail.com>
2018-11-05 18:57:05 +0300

    transactions view

--- pages/_network/block/_key.vue
+++ pages/_network/block/_key.vue
@@ -7,18 +7,19 @@
           m-hash.is-size-1(:value='block.hash' :head='0' :tail='0')
             
         t-container.tile.is-child.has-background-primary.has-text-white-bis(:padding='30')
-      .tile
+      .tile.has-background-white-ter
         t-container.tile.is-child(:padding='30')
           h3.is-size-3 Core
           o-data-table(:dataset='block' :schema='details')
 
-        t-container.tile.is-child.has-background-white-ter(:padding='30')
+        t-container.tile.is-child(:padding='30')
           h3.is-size-3 Ethereum specific
           o-data-table(:dataset='block' :schema='specific')
 
-      t-container.tile.is-child.has-background-white-ter(:padding='30')
+      t-container.tile.is-child(:padding='30')
         h3.is-size-3 Transactions
 
+        o-data-table(:dataset='block.transactions' :schema='showTx')
 </template>
 
 <script lang="ts">
@@ -30,6 +31,8 @@ import { IBlock } from "store";
 import ContainerVue from "~/components/templates/Container.vue";
 import HashVue from "~/components/moleculas/data-table-fields/Hash.vue";
 import DataTableVue from "~/components/organisms/DataTable.vue";
+
+import bn from "bn.js";
 
 @Component(<any>{
   name: "l-page",
@@ -50,13 +53,15 @@ import DataTableVue from "~/components/organisms/DataTable.vue";
     ) {
       // hash
       hash = key.startsWith("0x") ? key.slice(2) : key;
-      block = await context.app.$connection.getBlockByHash(hash);
+      block = await context.app.$connection.getBlockByHash(hash, true);
     } else if (/^\d+$/.test(key)) {
       height = parseInt(key);
-      block = await context.app.$connection.getBlockByHeight(height);
+      block = await context.app.$connection.getBlockByHeight(height, true);
     } else {
       throw new Error("incorrect block identity");
     }
+
+    console.log(block);
 
     return {
       block,
@@ -137,7 +142,8 @@ export default class extends Vue {
       },
       {
         title: "Tx Count",
-        key: row => row.transactions.length,
+        key: row =>
+          Array.isArray(row.transactions) ? row.transactions.length : 0,
         width: 65
       },
       {
@@ -176,25 +182,66 @@ export default class extends Vue {
       }
     ];
   }
+
+  fromWei(a) {
+    return (
+      Array(18)
+        .fill(0)
+        .join("") + a
+    )
+      .replace(/^(\d*)(\d{18})$/, "$1.$2")
+      .replace(/^0*/, "")
+      .replace(/0*$/, "")
+      .replace(/^\./, "0.")
+      .replace(/\.$/, "");
+  }
+
+  get showTx() {
+    return [
+      {
+        title: "Hash",
+        key: "hash",
+        editor: "hash"
+      },
+      {
+        title: "Value",
+        key: row =>
+          this.fromWei(new bn(row.value.slice(2), 16).toString(10)) + " ETH"
+      },
+      {
+        title: "From",
+        key: "from",
+        editor: "hash",
+        props: {
+          head: 2,
+          tail: 4
+        }
+      },
+      {
+        title: "To",
+        key: "to",
+        editor: "hash",
+        props: {
+          head: 2,
+          tail: 4
+        }
+      },
+      {
+        title: "Fee",
+        key: row =>
+          this.fromWei(
+            new bn(row.gas.slice(2), 16)
+              .mul(new bn(row.gasPrice.slice(2), 16))
+              .toString(10)
+          ) + " ETH"
+      }
+    ];
+  }
 }
 </script>
 
 <style lang="scss">
 .l-page {
-  &__spec {
-    display: flex;
-
-    &-term {
-      white-space: nowrap;
-      margin-right: auto;
-    }
-
-    &-desc {
-      text-overflow: ellipsis;
-      overflow: hidden;
-      margin-left: 20px;
-    }
-  }
 }
 </style>
 
