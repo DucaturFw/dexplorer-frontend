3f4723691dae9705207f1cb799013c5a096e7ae5
Aler Denisov <aler.zampillo@gmail.com>
2018-11-06 10:07:35 +0300

    remove dirty logs

--- middleware/network.ts
+++ middleware/network.ts
@@ -3,9 +3,6 @@ export default function({ isHMR, app, store, route, params, redirect }) {
   if (isHMR) return;
   const networks = ["eth", "btc", "eos", "neo", "ada"];
   const net = params.network || route.fullPath.split("/")[1];
-  console.log("lang", net);
-  console.log("param", params.network);
-  console.log("full", route.fullPath);
   if (!net || networks.indexOf(net) === -1) {
     return redirect(`/${defaultNetwork}`);
   } else {
